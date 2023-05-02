34a6687c09c14392ada7ad0295c6a9cd98d40a05
wiamoh <wsoftenginer02@gmail.com>
2023-05-02 09:30:03 -0700

    tst

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -14,7 +14,7 @@ char tmp;
 while (s[index++])
 len++;
 
-for (index = len - 1; index > = len / 2; index--)
+for (index = len - 1; index >= len / 2; index--)
 {
 tmp = s[index];
 s[index] = s[len - index - 1];
