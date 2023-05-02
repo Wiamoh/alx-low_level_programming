a3187bfd923f1ff705bd1a826e1ab3acb9872401
wiamoh <wsoftenginer02@gmail.com>
2023-05-02 09:18:39 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,21 +3,21 @@
 
 /**
  * rev_string - Reverses a string
- * @s: Input string
- * Return: String in reverse
+ * @s: The string to be modified
+ * Return: void
  */
 void rev_string(char *s)
 {
-char rev = s[0];
-int counter = 0;
-int i;
-while (s[counter] != '\0')
-counter++;
-for (i = 0; i < counter; i++)
+int len = 0, index = 0;
+char tmp;
+
+while (s[index++])
+len++;
+
+for (index = len - 1; index > = len / 2; index--)
 {
-counter--;
-rev = s[i]
-s[i] = s[counter];
-s[counter] = rev;
+tmp = s[index];
+s[index] = s[len - index - 1];
+s[len - index - 1] = tmp;
 }
 }
