6d2ac78ae1ddecbfee8474f3b2dbd129c54a19d8
wiamoh <wsoftenginer02@gmail.com>
2023-05-12 13:00:25 -0700

    task4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -41,12 +41,10 @@ while (count < argc) /*Goes through the whole array*/
 {
 if (check_num(argv[count]))
 {
-str_to_int = atoi(argv[count]); /*ATOI --> convert string
-to int*/
+str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 sum += str_to_int;
 }
-/*Condition if one of the number contains symbols that are
-not digits*/
+/*Condition if one of the number contains symbols that are not digits*/
 else
 {
 printf("Error\n");
@@ -56,4 +54,4 @@ count++;
 }
 printf("%d\n", sum); /*print sum*/
 return (0);
-}
+}}
